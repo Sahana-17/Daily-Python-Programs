befcfc21cda522db89a83992a43d1c64ed5673c7
Sahana Muralikrishnan <hola.sahana@gmail.com>
2021-07-01 15:32:47 +0530

    adding floyd triangle

---
+++ floyd_triangle.py
@@ -0,0 +1,8 @@
+line = int(input("Enter number of lines : "))
+num = 1
+
+for i in range(1, line + 1):
+    for j in range(1, i + 1):        
+        print(num, end = '  ')
+        num = num + 1
+    print()
