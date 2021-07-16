70eec897aaaa5a318db75476ec64bb6875b671eb
Sahana Muralikrishnan <hola.sahana@gmail.com>
2021-07-16 12:12:00 +0530

    changes to caesar cipher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,11 +4,17 @@ def cipher(plain_text, shift):
 	y = len(plain_text)
 	for i in range(y):  
 		char = plain_text[i]  
-        
-		if (char.isupper()):  
-			result += chr((ord(char) + shift - 64) % 26 + 64)  
-		else:  
-			result += chr((ord(char) + shift - 96) % 26 + 96)  
+		flag = char.isalpha() 
+
+		if flag == True:
+
+			if (char.isupper()):  
+				result += chr((ord(char) + shift - 64) % 26 + 64)  
+			else:  
+				result += chr((ord(char) + shift - 96) % 26 + 96)  
+
+		else:
+			result += char
 	return result  
   
 plain_text = input("Enter string : ")  
